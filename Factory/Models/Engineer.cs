ee7da258662a49b06e4bc08798fe211dea5ad638
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-02-09 17:32:55 -0800

    added base files, set up models and did initial migration and set up database

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace Factory.Models;
+
+public class Engineer
+{
+  public int EngineerId { get; set; }
+  public string Name { get; set; }
+  public string Phone { get; set; }
+  public DateTime HireDate { get; set; }
+  public bool IsActive {  get; set; }
+  public string Renewal { get; set; }
+  public List<EngineerMachine> JoinEntities { get; } 
+}
