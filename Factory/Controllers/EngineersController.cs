51e8e7e2898294b11e2211f32a45830b3c6e6111
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-02-09 21:31:34 -0800

    Engineer Controller WIP

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -23,5 +23,62 @@ namespace Factory.Controllers
       List<Engineer> model = _db.Engineers.ToList();
       return View(model);
     }
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Engineer engineer)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(engineer);
+      }
+      else
+      {
+        _db.Engineers.Add(engineer);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+    }
+    public ActionResult Details(int id)
+    {
+      Engineer thisEngineer = _db.Engineers
+      .Include(engineer => engineer.JoinEntities)
+      .ThenInclude(join => join.Machine)
+      .FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
+
+    public ActionResult AddMachine(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Name");
+      return View(thisEngineer);
+    }
+    [HttpPost]
+    public ActionResult AddMachine(Engineer engineer, int[] machineIds)
+    {
+      if (machineIds != null && machineIds.Length > 0)
+      {
+        foreach (var machineId in machineIds)
+        {
+          #nullable enable
+          EngineerMachine? joinEntity = _db.EngineerMachines.FirstOrDefault(join => join.MachineId == machineId && join.EngineerId == engineer.EngineerId);
+          #nullable disable
+          if (joinEntity == null)
+          {
+            _db.EngineerMachines.Add(new EngineerMachine() { MachineId = machineId, EngineerId = engineer.EngineerId });
+          }
+          _db.SaveChanges();
+        }
+        return RedirectToAction("Details", new { id = engineer.EngineerId });
+      }
+      else
+      {
+        return RedirectToAction("Index", "Home");
+      }
+    }
   }
 }
