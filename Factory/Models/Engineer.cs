15d7f0edc5aa0a5ffb692d0fb788c4b47ddb5541
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-02-09 22:32:50 -0800

    updated  machine/engineer index and engineer create file

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -8,7 +8,7 @@ public class Engineer
   public int EngineerId { get; set; }
   public string Name { get; set; }
   public string Phone { get; set; }
-  public DateTime HireDate { get; set; }
+  public DateTime HireDate { get; set; } = DateTime.Now;
   public bool IsActive {  get; set; }
   public string Renewal { get; set; }
   public List<EngineerMachine> JoinEntities { get; } 
