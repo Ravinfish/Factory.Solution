4a853deb6b17f933facf8083f9c05225620f5e0d
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-02-19 07:15:58 -0800

    added delete join function and fixed error handling

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models;
 
 public class Engineer
 {
   public int EngineerId { get; set; }
+  [Required(ErrorMessage = "You must enter a name")]
   public string Name { get; set; }
+  [Required(ErrorMessage = "You must enter a phone number")]
   public string Phone { get; set; }
   public DateTime HireDate { get; set; } = DateTime.Now;
-  public bool IsActive {  get; set; }
+  public bool IsActive { get; set; }
+  [Required(ErrorMessage = "You must enter your license expiration date")]
   public string Renewal { get; set; }
-  public List<EngineerMachine> JoinEntities { get; } 
+  public List<EngineerMachine> JoinEntities { get; }
 }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models;
 
 public class Machine
 {
   public int MachineId { get; set; }
+  [Required(ErrorMessage = "You must enter a name")]
   public string Name { get; set; }
+  [Required(ErrorMessage = "You must enter a description")]
   public string Description { get; set; }
-  public DateTime InstallDate { get; set; }
+  public DateTime InstallDate { get; set; } = DateTime.Now;
   public string Inspection {  get; set; }
   public string Status { get; set; }
   public List<EngineerMachine> JoinEntities { get; } 
